861e43447b2b5c4c246ac25e61be62dd5730e98c
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-21 21:18:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = :known_for => "Ruby", :language => ["Liso", "C"]
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :language => ["Liso", "C"]}
     
 end
 
